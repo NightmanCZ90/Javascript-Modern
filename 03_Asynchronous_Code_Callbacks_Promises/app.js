2e7bcb4f85f512dac2e6d0e1448f9e16b8ff5ae7
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-04 00:39:06 +0200

    Callback Hell

--- 03_Asynchronous_Code_Callbacks_Promises/app.js
+++ 03_Asynchronous_Code_Callbacks_Promises/app.js
@@ -1,7 +1,46 @@
-console.log("I happen first");
+const btn = document.querySelector('button');
 
-setTimeout(function() {
-	console.log("I happen third")
-}, 3000)
+// setTimeout(() => {
+// 	btn.style.transform = `translateX(100px)`;
+// 	setTimeout(() => {
+// 		btn.style.transform = `translateX(200px)`;
+// 		setTimeout(() => {
+// 			btn.style.transform = `translateX(300px)`;
+// 			setTimeout(() => {
+// 				btn.style.transform = `translateX(400px)`;
+// 				setTimeout(() => {
+// 					btn.style.transform = `translateX(500px)`;
+// 				}, 1000)
+// 			}, 1000)
+// 		}, 1000)
+// 	}, 1000)
+// }, 1000)
 
-console.log("I happen second");
+const moveX = (element, amount, delay, successCallback, failCallback) => {
+	setTimeout(() => {
+		const maxX = document.body.clientWidth;
+		const dimensions = element.getBoundingClientRect();
+		const currentRight = dimensions.x + dimensions.width;
+		if(currentRight + amount > maxX) {
+			failCallback();
+		}
+		else {
+			element.style.transform = `translateX(${currentRight + amount}px)`;
+			successCallback();
+		}
+	}, delay)
+};
+
+ moveX(btn, 100, 1000, () => {
+	moveX(btn, 200, 1000, () => {
+		moveX(btn, 300, 1000, () => {
+			moveX(btn, 400, 1000, () => {
+				moveX(btn, 500, 1000, () => {
+					moveX(btn, 600, 1000);
+				 });
+			 });
+		 });
+	 });
+ });
+
+ 
