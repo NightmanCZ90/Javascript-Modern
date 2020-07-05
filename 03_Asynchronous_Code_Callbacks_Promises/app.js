1942e68f5c7362e8e0a2b95fe597bcc4711352cd
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-05 22:30:42 +0200

    Axios

--- 03_Asynchronous_Code_Callbacks_Promises/app.js
+++ 03_Asynchronous_Code_Callbacks_Promises/app.js
@@ -63,59 +63,59 @@ const btn = document.querySelector('button');
 // 		console.log("NOPE")
 // 	})
 
-const fakeRequest = (url) => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-				const pages = {
-					'/users': [
-						{id: 1, username: "Bilbo"},
-						{id: 5, username: "Esmeralda"}
-					],
-					'/users/1': {
-						id				:	1,
-						username	:	'Bilbo',
-						upvotes		:	360,
-						city			:	'Lisbon',
-						topPostId	:	454321
-					},
-					'/users/5': {
-						id				:	5,
-						username	:	'Esmeralda',
-						upvotes		:	571,
-						city			:	'Honolulu'
-					},
-					'/posts/454321': {
-						id				:	454321,
-						title			:	'Ladies & Gentlemen, may I introduce my pet pig, Hamlet.',
-					},
-					'/about': 'This is the about page'
-				};
-				const data = pages[url];
-				if(data) {
-					resolve({status: 200, data});
-				} else {
-					reject({status: 404});
-				}
-		}, 1000)
-	})
-};
+// const fakeRequest = (url) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 				const pages = {
+// 					'/users': [
+// 						{id: 1, username: "Bilbo"},
+// 						{id: 5, username: "Esmeralda"}
+// 					],
+// 					'/users/1': {
+// 						id				:	1,
+// 						username	:	'Bilbo',
+// 						upvotes		:	360,
+// 						city			:	'Lisbon',
+// 						topPostId	:	454321
+// 					},
+// 					'/users/5': {
+// 						id				:	5,
+// 						username	:	'Esmeralda',
+// 						upvotes		:	571,
+// 						city			:	'Honolulu'
+// 					},
+// 					'/posts/454321': {
+// 						id				:	454321,
+// 						title			:	'Ladies & Gentlemen, may I introduce my pet pig, Hamlet.',
+// 					},
+// 					'/about': 'This is the about page'
+// 				};
+// 				const data = pages[url];
+// 				if(data) {
+// 					resolve({status: 200, data});
+// 				} else {
+// 					reject({status: 404});
+// 				}
+// 		}, 1000)
+// 	})
+// };
 
-fakeRequest('/users').then((res) => {
-	console.log(res)
-	const id = res.data[0].id;
-	return fakeRequest(`/users/${id}`)
-})
-.then((res) => {
-	console.log(res)
-	const postId = res.data.topPostId;
-	return fakeRequest(`/posts/${postId}`)
-})	
-.then((res) => {
-	console.log(res.data.title)
-})
-.catch((err) => {
-	console.log("OH NO", err)
-})
+// fakeRequest('/users').then((res) => {
+// 	console.log(res)
+// 	const id = res.data[0].id;
+// 	return fakeRequest(`/users/${id}`)
+// })
+// .then((res) => {
+// 	console.log(res)
+// 	const postId = res.data.topPostId;
+// 	return fakeRequest(`/posts/${postId}`)
+// })	
+// .then((res) => {
+// 	console.log(res.data.title)
+// })
+// .catch((err) => {
+// 	console.log("OH NO", err)
+// })
 // .catch((res) => {
 // 	console.log(res.status, res.name);
 // 	console.log("REJECTED")
@@ -133,11 +133,18 @@ fakeRequest('/users').then((res) => {
 
 // btn.addEventListener("onclick", fakeRequest());
 
-moveX(btn, 300, 1000).then(() => {return moveX(btn, 300, 1000)})
-.then(() => moveX(btn, 300, 1000))
-.then(() => moveX(btn, 300, 1000))
-.then(() => moveX(btn, 300, 1000))
-.then(() => moveX(btn, 300, 1000))
-.then(() => moveX(btn, 300, 1000))
-.then(() => {console.log("DONE MOVING")})
-.catch(() => {console.log("NOT ENOUGH SPACE")})
+// moveX(btn, 300, 1000).then(() => {return moveX(btn, 300, 1000)})
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => {console.log("DONE MOVING")})
+// .catch(() => {console.log("NOT ENOUGH SPACE")})
+
+axios.get('https://swapi.dev/api/planets/').then((res) => {
+	console.log(res.data);
+}).catch((err) => {
+	console.log("In Catch Callback!")
+	console.log(err)
+})
