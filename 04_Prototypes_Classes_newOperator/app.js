dffc0a9582bbdb219e4b8702371d233b9465f5a6
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-06 23:22:13 +0200

    class structure - syntactical sugar

--- 04_Prototypes_Classes_newOperator/app.js
+++ 04_Prototypes_Classes_newOperator/app.js
@@ -23,28 +23,54 @@
 // 	return color;
 // }
 
-function Color(r,g,b){
-	this.r = r;
-	this.g = g;
-	this.b = b;
-}
+// function Color(r,g,b){
+// 	this.r = r;
+// 	this.g = g;
+// 	this.b = b;
+// }
 
-Color.prototype.rgb = function() {
-	const {r,g,b} = this;
-	return `rgb(${r}, ${g}, ${b})`;
+// Color.prototype.rgb = function() {
+// 	const {r,g,b} = this;
+// 	return `rgb(${r}, ${g}, ${b})`;
 
-}
+// }
 
-Color.prototype.hex = function() {
-	const {r,g,b} = this;
-	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+// Color.prototype.hex = function() {
+// 	const {r,g,b} = this;
+// 	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
 
-Color.prototype.rgba = function(alfa=1.0) {
-	const {r,g,b} = this;
-	return `rgba(${r}, ${g}, ${b}, ${alfa})`;
-}
+// Color.prototype.rgba = function(alfa=1.0) {
+// 	const {r,g,b} = this;
+// 	return `rgba(${r}, ${g}, ${b}, ${alfa})`;
+// }
 
-const color1 = new Color(126, 45, 230);
+// const color1 = new Color(126, 45, 230);
 
-"rgba(126, 45, 230, 0.2)"
+class Color {
+	constructor(r, g, b, name) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.name = name;
+	};
+	innerRGB() {
+		const { r, g, b } = this;
+		return `${r}, ${g}, ${b}`;
+	}
+	rgb() {
+		return `rgb(${this.innerRGB()})`;
+	};
+	rgba = function(alfa=1.0) {
+		const { r, g, b } = this;
+		return `rgba(${this.innerRGB()}, ${alfa})`;
+	};
+	hex = function() {
+		const { r, g, b } = this;
+		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	};
+};
+const color1 = new Color(255, 10, 20, "tomato");
+
+"rgb(255, 10, 20)"
+"rgba(255, 10, 20, 0.7)"
