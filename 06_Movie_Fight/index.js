d02f8f713ea179740c5cc512cdd00efc351ca7c7
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-09 00:43:51 +0200

    add search code and debounce function

--- 06_Movie_Fight/index.js
+++ 06_Movie_Fight/index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'fc7091e3',
+      s: searchTerm
+    }
+  });
+  console.log(response.data)
+};
+
+const input = document.querySelector('input');
+
+
+
+const onInput = event => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
