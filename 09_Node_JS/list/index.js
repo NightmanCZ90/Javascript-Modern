ac588c6916839319b1a97c205f7c8f8787487a4a
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-15 02:57:08 +0200

    add 2 promise callbacks

--- 09_Node_JS/list/index.js
+++ 09_Node_JS/list/index.js
@@ -3,36 +3,44 @@
 //had troubles with npm link... added the relative path of AppData/Roaming/npm into PATH in environments
 
 const fs = require('fs'); // file system module
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+//////////////////////////////////////////////////////
+// Method #2 - CALLBACK BASED FUNCTIONS USING PROMISES
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3 - another with promises
+const { lstat } = fs.promises;
+
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  // const allStats = Array(filenames.length).fill(null);
-
-  // for (let filename of filenames) {
-  //   const index = filenames.indexOf(filename);
-
-  //   fs.lstat(filename, (err, stats) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-
-  //     allStats[index] = stats;
-
-  //     const ready = allStats.every((stats) => {
-  //       return stats;
-  //     });
-
-  //     if (ready) {
-  //       allStats.forEach((stats, index) => {
-  //         console.log(filenames[index], stats.isFile());
-  //       });
-  //     }
-  //   });
-  // }
-
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
   
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
+
+//////////////////////////////////////
+// Method #1 - CALLBACK-BASED SOLUTION
+
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
 
